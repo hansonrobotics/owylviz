39338a3b4f4bc3a2cfbca0aa6970df927059b16d
Gabrielius Mickevicius <gabrielius.m@gmail.com>
2015-02-04 15:26:31 +0200

    Index/status page. Functional backend, stub frontend.

--- server/index.js
+++ server/index.js
@@ -3,40 +3,52 @@ var http = require('http').Server(app);
 var io = require('socket.io');
 io = io.listen(http);
 
+var trees = {};
+
+//Connection from browser at home
+var statusio = io.of('/status').on('connection', function(socket){
+  socket.emit('rooms', Object.keys(trees));
+});
+
+//Connection from browser at some tree page
+var displayio = io.of('/display').on('connection', function(socket){
+  socket.on('join room', function(room) {
+    socket.join(room);
+    socket.emit('tree', trees[room]);
+  });
+});
+
+//Connection from owylviz
+io.of('/accept').on('connection', function(socket) {
+  var room;
+
+  socket.on('introduce', function(_room, structure) {
+    room = _room;
+    trees[room] = structure;
+    displayio.in(room).emit('tree', structure);
+    statusio.emit('rooms', Object.keys(trees));
+  });
+
+  socket.on('step', function(id) {
+    displayio.in(room).emit('step', id);
+  });
+
+  socket.on('disconnect', function() {
+    delete trees[room];
+    statusio.emit('rooms', Object.keys(trees));
+  });
+
+});
+
+
 __dirname += '/public';
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html')
 });
 
-app.get('/tree/*', function(req, res){
+app.get('/*', function(req, res){
   res.sendFile(__dirname + '/tree.html');
-});
-
-var trees = {};
-
-//Connection from owylviz
-io.of('/accept').on('connection', function(socket) {
-  var room;
-  socket.on('introduce', function(_room, structure) {
-    room = _room;
-    trees[room] = structure;
-    display.in(room).emit('tree', structure);
-  });
-  socket.on('step', function(id) {
-    display.in(room).emit('step', id);
-  });
-  socket.on('disconnect', function() {
-    delete trees[room];
-  });
-});
-
-//Connection from browser
-var display = io.of('/display').on('connection', function(socket){
-  socket.on('join room', function(room) {
-    socket.join(room);
-    socket.emit('tree', trees[room]);
-  });
 });
 
 app.set('port', (process.env.PORT || 3000))
